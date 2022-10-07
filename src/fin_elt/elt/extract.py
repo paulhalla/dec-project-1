b6a4fe5ac9b909cee4e5c734856adf0df654fe42
Paul Hallaste <peeterpaul.hallaste@gmail.com>
2022-10-07 09:08:29 +0900

    refactored extract fx code

--- src/fin_elt/elt/extract.py
+++ src/fin_elt/elt/extract.py
@@ -44,29 +44,41 @@ class Extract:
                 logging.error('Call to API - treasury yields - failed.')
 
     @staticmethod
-    def extract_fx_rate(
-        from_symbol:str,
-        to_symbol:str,
-        api_key:str
+    def fx_rate(
+            to_symbol:str,
+            api_key:str
         )->pd.DataFrame:
         """
         Extracts daily exchange rates.
         - `from_symbol`: The currency symbol from which the exchange occurs.
         - `to_symbol`: The currency symbol into which the exchange occurs.
-        
         """
-        base_url = f'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=USD&to_symbol={to_symbol}&outputsize=full&apikey={api_key}'
-        r = requests.get(base_url)
-        if r.status_code == 200:
-            response_data = r.json()
-            df = pd.DataFrame(response_data['Time Series FX (Daily)']).transpose().reset_index()
-            df = df.rename(columns={ df.columns[0]: "Date" })
-            # df['from'] = f'{from_symbol}' # Won't use in case we do only USD in the FROM column
-            df['to'] = f'{to_symbol}'
-            return df
-    
+        logging.basicConfig(level=logging.INFO, format="[%(levelname)s][%(asctime)s]: %(message)s")
+        
+        url = f'https://www.alphavantage.co/query?'
+              f'function=FX_DAILY&'
+              f'from_symbol=USD&'
+              f'to_symbol={to_symbol}&'
+              f'outputsize=full&'
+              f'apikey={api_key}'
+            
+        if api_key:
+            r = requests.get(url)
+            if r.status_code == 200:
+                try:
+                    response_data = r.json()
+                    df = pd.DataFrame(response_data['Time Series FX (Daily)']).transpose().reset_index()
+                    df = df.rename(columns={ df.columns[0]: "Date" })
+                    # df['from'] = f'{from_symbol}' # Won't use in case we do only USD in the FROM column
+                    df['to'] = f'{to_symbol}'
+                    return df
+                except KeyError:
+                    logging.error(f'Error extracting exchange rates to {to_symbol} from API')
+            else:
+                logging.error('Call to API - exchange rates - failed.')
+                
     @staticmethod
-    def extract_several_fx_rates():
+    def several_fx_rates():
         df_currencies = pd.read_csv("data/main_currencies.csv")
         df_concat = pd.DataFrame()
         for currency_name in df_currencies["currency code"]:
